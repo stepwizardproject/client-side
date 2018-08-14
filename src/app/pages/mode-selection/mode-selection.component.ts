d78678234228138d76716ae10335b32e6f9aba2e
ZaharchenkoDmitriy <imdrchaka@gmail.com>
2018-08-15 01:01:04 +0300

    add switch state after choose the mode

--- src/app/pages/mode-selection/mode-selection.component.ts
+++ src/app/pages/mode-selection/mode-selection.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {StateService} from '../../services/state.service';
 
 @Component({
   selector: 'app-mode-selection',
@@ -7,12 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModeSelectionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private stateService = new StateService()) { }
 
   ngOnInit() {
   }
 
   chooseMode(mode: string): any {
-
+    this.stateService.nextState();
   }
 }
