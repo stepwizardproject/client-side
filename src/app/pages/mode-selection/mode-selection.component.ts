84cb59cd6cb936855c60fff7959cfa9a54df4f40
ZaharchenkoDmitriy <imdrchaka@gmail.com>
2018-08-15 01:06:29 +0300

    fix service injection

--- src/app/pages/mode-selection/mode-selection.component.ts
+++ src/app/pages/mode-selection/mode-selection.component.ts
@@ -8,7 +8,7 @@ import {StateService} from '../../services/state.service';
 })
 export class ModeSelectionComponent implements OnInit {
 
-  constructor(private stateService = new StateService()) { }
+  constructor(private stateService: StateService) { }
 
   ngOnInit() {
   }
